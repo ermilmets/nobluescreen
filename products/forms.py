e89309c39bcd1f6c8b079d9a2a8a207dad578679
Ermil <ermilmets@gmail.com>
2024-07-21 12:39:21 +0300

    add cart/checkout

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,28 @@
 from django import forms
-from products.models import ProductRating
+from products.models import ProductRating, Order
 
 
 class ProductRatingForm(forms.ModelForm):
     class Meta:
         model = ProductRating
         fields = ['feedback', 'star_rating']
+
+
+class CheckoutForm(forms.Form):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}))
+
+
+# class OrderForm(forms.ModelForm):
+#     class Meta:
+#         model = Order
+#         fields = '__all__'
+
+
+
 
 
 
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView, FormView, ListView
-from products.models import *
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, FormView, ListView, View
+from products.models import *
 from products.forms import *
 
 
@@ -42,11 +43,11 @@ class ProductDetailView(DetailView, FormView):
     def post(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect('login')  # reverse_lazy('login')
-        self.object = self.get_object()
+        user_feedback = self.get_object()
         form = self.get_form()
         if form.is_valid():
             star_rating, created = ProductRating.objects.update_or_create(
-                product=self.object, user=request.user,
+                product=user_feedback, user=request.user,
                 defaults={'star_rating': form.cleaned_data['star_rating'],
                           'feedback': form.cleaned_data['feedback']})
             return self.form_valid(form)
@@ -55,7 +56,7 @@ class ProductDetailView(DetailView, FormView):
 
     def form_valid(self, form):
         # form.save()
-        return redirect('product_detail', pk=self.object.id)   # product_id=self.get_object().id
+        return redirect('product_detail', pk=self.get_object().id)   # product_id=self.get_object().id
 
 
 class GamePlatformView(ListView):  # based on chosen category, shows games only on chosen platform
@@ -64,17 +65,88 @@ class GamePlatformView(ListView):  # based on chosen category, shows games only 
     context_object_name = 'products'
 
     def get_queryset(self):
-        platform = self.kwargs['platform']
-        self.platform = get_object_or_404(Platform, slug=platform)
-        return Product.objects.filter(platform=self.platform)
+        selected_platform = self.kwargs.get('platform', 'PS5')
+        platform_object = get_object_or_404(Platform, slug=selected_platform)
+        return Product.objects.filter(platform=platform_object)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # context['platform'] = Platform.objects.all()
-        context['platform'] = self.platform
+        context['platform'] = self.kwargs.get('platform')  # self.platform
         context['product_ratings'] = ProductRating.objects.all()
 
         return context
+
+
+class AddToCartView(View):
+    @method_decorator(login_required(redirect_field_name='cart_detail'))  # 'next'
+    def post(self, request, product_id):
+        product = get_object_or_404(Product, pk=product_id)
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+        if not created:
+            cart_item.quantity += 1
+            cart_item.save()
+        return redirect('cart_detail')   # (, pk=cart_item.pk)
+
+
+class RemoveFromCartView(View):
+    @method_decorator(login_required)
+    def post(self, request, product_id):  # or post
+        product = get_object_or_404(Product, pk=product_id)
+        cart = get_object_or_404(Cart, user=request.user)
+        cart_item = get_object_or_404(CartItem, cart=cart, product=product)  # cart__user=request.user
+
+        if cart_item.quantity == 1:
+            cart_item.delete()
+        else:
+            cart_item.quantity -= 1
+            cart_item.save()
+
+        return redirect('cart_detail')
+
+
+class CartDetailView(DetailView):  # LoginRequiredMixin,
+    # model = Cart
+    template_name = 'cart_detail.html'
+    # context_object_name = 'cart'
+
+    @method_decorator(login_required(redirect_field_name='cart_detail'))
+    def get(self, request, *args, **kwargs):
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        return render(request, self.template_name, {'cart': cart})
+
+
+class CheckoutView(View):
+    template_name = 'checkout.html'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        form = CheckoutForm(request.POST or None)
+        return render(request, self.template_name, {'form': form})
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = CheckoutForm(request.POST or None)
+        if form.is_valid():
+            # cart = Cart.objects.get_or_create(user=request.user)
+            cart = get_object_or_404(Cart, user=request.user)
+            # dummy order processing
+            cart.products.clear()
+            return redirect('checkout_success')
+
+
+class CheckoutSuccessView(View):
+    template_name = 'checkout_success.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request,self.template_name)
+
+
+# class CartItemView(ListView):
+#     model = Product
+#     template_name = 'cart_detail.html'
+#     context_object_name = 'products'
 
 
 class CreateFeedbackView(CreateView, LoginRequiredMixin):
